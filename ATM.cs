0fc9895314894c576c42773dbd0cfad41550fb03
Егор Климочкин <fix2605@mail.ru>
2024-04-10 13:19:25 +0200

    Added a separate class

---
+++ ATM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_machine_TestTask
+{
+    class ATM
+    {
+        private Dictionary<int, int> banknotes = new Dictionary<int, int>();
+
+        public ATM(string filename) 
+        {
+            banknotes = LoadBanknotes(filename);
+        }
+
+        private static Dictionary<int, int> LoadBanknotes(string filename)
+        {
+            return File.ReadAllLines(filename)
+                       .Select(line => line.Split('-'))
+                       .ToDictionary(parts => int.Parse(parts[0]), parts => int.Parse(parts[1]));
+        }
+
+        public Dictionary<int, int>? WithdrawAmount(int remainingAmount)
+        {
+            var withdrawal = new Dictionary<int, int>();
+            var availableBanknotes = banknotes.OrderByDescending(kv => kv.Key);
+
+            foreach (var (nominal, count) in availableBanknotes)
+            {
+                var withdrawnCount = Math.Min(remainingAmount / nominal, count);
+                if (withdrawnCount > 0)
+                {
+                    withdrawal[nominal] = withdrawnCount;
+                    remainingAmount -= withdrawnCount * nominal;
+                }
+            }
+
+            if (remainingAmount == 0)
+            {
+                foreach (var (nominal, count) in withdrawal)
+                    banknotes[nominal] -= count;
+                return withdrawal;
+            }
+            else
+                return null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,42 +2,10 @@
 {
     internal class Program
     {
-        static Dictionary<int, int> LoadBanknotes(string filename)
-        {
-            return File.ReadAllLines(filename)
-                       .Select(line => line.Split('-'))
-                       .ToDictionary(parts => int.Parse(parts[0]), parts => int.Parse(parts[1]));
-        }
-
-        static Dictionary<int, int>? WithdrawAmount(int remainingAmount, Dictionary<int, int> banknotes)
-        {
-            var withdrawal = new Dictionary<int, int>();
-            var availableBanknotes = banknotes.OrderByDescending(kv => kv.Key);
-
-            foreach (var (nominal, count) in availableBanknotes)
-            {
-                var withdrawnCount = Math.Min(remainingAmount / nominal, count);
-                if (withdrawnCount > 0)
-                {
-                    withdrawal[nominal] = withdrawnCount;
-                    remainingAmount -= withdrawnCount * nominal;
-                }
-            }
-
-            if (remainingAmount == 0)
-            {
-                foreach (var (nominal, count) in withdrawal)
-                    banknotes[nominal] -= count;
-                return withdrawal;
-            }
-            else
-                return null;
-        }
-
         static void Main(string[] args)
         {
             var banknotesFile = "../../../banknotes.txt";
-            var banknotes = LoadBanknotes(banknotesFile);
+            var atm = new ATM(banknotesFile);
 
             while (true)
             {
@@ -45,7 +13,7 @@
                 {
                     Console.Write("Введите сумму для снятия: ");
                     var amount = int.Parse(Console.ReadLine());
-                    var withdrawal = WithdrawAmount(amount, banknotes);
+                    var withdrawal = atm.WithdrawAmount(amount);
                     if (withdrawal != null)
                     {
                         Console.WriteLine("Выдано следующее количество купюр:");
